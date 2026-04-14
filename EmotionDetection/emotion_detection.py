9c7b9a7113c4039cde55f51bc3d9463af8c83948
acker3125-ctrl <acker3125@gmail.com>
2026-04-14 02:40:18 -0400

    Error Handling partial implementation

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -14,21 +14,32 @@ def emotion_detector(text_to_analyse):
     #Pass the request to Watson and collect the response
     response = requests.post(url, json = myobj, headers=header)
     
-    #Format the response
-    formatted_response = json.loads(response.text)
+    #Setting to manually test error handling.
+    test_mode = True
 
-    #Filter through the response to the isolate the emotion scores.
-    emotion_scores = formatted_response['emotionPredictions'][0]["emotion"]
+    #If statement to assess whether to return the error response or proceed with formatting.
+    if response.status_code == 400 or test_mode == True:
+        emotion_scores = {'anger' : 'None', 'disgust' : 'None', 'fear' : 'None',
+        'joy' : 'None', 'sadness' : 'None', 'dominant_emotion' : 'None'}
+    else:
+        #Format the response
+        formatted_response = json.loads(response.text)
+
+        #Filter through the response to the isolate the emotion scores.
+        emotion_scores = formatted_response['emotionPredictions'][0]["emotion"]
     
-    #Initialize placeholders for the top score
-    top_score = 0
-    dominant_emotion = 'null'
-    #Loop through the raw scores to find the dominant emotion
-    for emotion, score in emotion_scores.items():
-        if score > top_score:
-            top_score = score
-            dominant_emotion = emotion
+        #Initialize placeholders for the top score
+        top_score = 0
+        dominant_emotion = 'None'
+        #Loop through the raw scores to find the dominant emotion
+        for emotion, score in emotion_scores.items():
+            if score > top_score:
+                top_score = score
+                dominant_emotion = emotion
     
-    #Append the dominant emotion to the scores dictionary and return it as an output.
-    emotion_scores['dominant_emotion'] = dominant_emotion
-    return emotion_scores
+
+        #Append the dominant emotion to the scores dictionary and return it as an output.
+        emotion_scores['dominant_emotion'] = dominant_emotion
+
+
+        return emotion_scores
